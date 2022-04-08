e63d53461292681c0cb9afb4c31768e76df68e76
Meghanath Nalawade <50794140+mr-compilerMS@users.noreply.github.com>
2022-04-08 19:51:26 +0530

    Drawer, events carousel

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,131 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  final List<String> imglist = [
+    'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1625621540023-d4940fa0a222?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80',
+    'https://images.unsplash.com/photo-1520352408661-83957c1379d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80',
+  ];
+
+  get children => null;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          child: const Text('Home'),
-          onPressed: () {
-            FirebaseAuth.instance.signOut();
-          },
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture:
+                  'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1961&q=80'
+                      .circularNetworkImage(),
+              accountName:
+                  'Sanna Marin'.text.make().scale(scaleValue: 1.4).px(18),
+              accountEmail: 'sannamarin@gmail.com'.text.make(),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                image: DecorationImage(
+                  image: NetworkImage(
+                      'https://media.istockphoto.com/photos/camouflage-cloth-texture-abstract-background-and-texture-for-design-picture-id1287561722?b=1&k=20&m=1287561722&s=170667a&w=0&h=1J3t8Ed8FF9Nem9kDlFpMI5_x8vrgoIUdgzK23iYC-A='),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Edit Profile'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            Row(
+              children: [
+                ListTile(
+                  title: Text('Log Out'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.white10,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: Icon(
+              Icons.person,
+              color: Colors.blueAccent,
+            ),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
+        ),
+      ),
+      body: Container(
+        child: Column(children: [
+          Padding(
+            padding: EdgeInsets.only(bottom: 20),
+            child: Text(
+              "Upcoming events",
+              style: TextStyle(
+                color: Colors.red[900],
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          CarouselSlider(
+            items: imglist
+                .map((item) => ClipRRect(
+                      borderRadius: BorderRadius.circular(16.0),
+                      child: Image.network(
+                        item,
+                        fit: BoxFit.cover,
+                        width: 1000,
+                      ),
+                    ))
+                .toList(),
+            options: CarouselOptions(
+              autoPlay: true,
+              aspectRatio: 2.0,
+              enlargeCenterPage: true,
+            ),
+          ),
+        ]),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Events',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Resources',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notification',
+          ),
+        ],
       ),
     );
   }
